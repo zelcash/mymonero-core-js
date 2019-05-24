e3b244f338c21877e3508affb950671c717cef91
TheTrunk <tadeas.kmenta@zel.cash>
2019-05-24 12:20:53 +0200

    up

--- monero_utils/monero_config.js
+++ monero_utils/monero_config.js
@@ -32,26 +32,26 @@ const JSBigInt = require("../cryptonote_utils/biginteger").BigInteger;
 //
 module.exports = {
 	// Number of atomic units in one unit of currency. e.g. 12 => 10^12 = 1000000000000
-	coinUnitPlaces: 12,
+	coinUnitPlaces: 6,
 
 	// Minimum number of confirmations for a transaction to show as confirmed
 	txMinConfirms: 10,
 
 	// Currency symbol
-	coinSymbol: "XMR",
+	coinSymbol: "XCASH",
 
 	// OpenAlias prefix
-	openAliasPrefix: "xmr",
+	openAliasPrefix: "xca",
 
 	// Currency name
-	coinName: "Monero",
+	coinName: "X-CASH",
 
 	// Payment URI Prefix
-	coinUriPrefix: "monero:",
+	coinUriPrefix: "xcash:",
 
 	// Dust threshold in atomic units
 	// 2*10^9 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-	dustThreshold: new JSBigInt("2000000000"),
+	dustThreshold: new JSBigInt("2000"),
 
 	// Maximum block number, used for tx unlock time
 	maxBlockNumber: 500000000,
